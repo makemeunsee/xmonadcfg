8b2ce71c41e2c2423a4463e349fe88e5b43beb0c
makemeunsee <makemeunsee@gmail.com>
2015-12-09 21:06:14 +0100

    laptop config v0

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageHelpers
+
+main = do
+    xmproc <- spawnPipe "~/bin/xmobar_restart"
+    xmonad $ defaultConfig
+        { terminal = "urxvt"
+        , modMask = mod4Mask
+        , borderWidth = 1
+        , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $ smartBorders $ layoutHook defaultConfig
+        --, layoutHook = smartBorders $ layoutHook defaultConfig
+        , startupHook = setWMName "LG3D"
+        }
+        `additionalKeysP`
+        [ ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
+        , ("<XF86AudioLowerVolume>", spawn "amixer set 'Master' 3%-")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer set 'Master' 3%+")
+        , ("<XF86Sleep>", spawn "sudo pm-suspend")
+        , ("M-l", spawn "slock")
+        , ("M-f", spawn "firefox")
+        , ("M-i", spawn "/home/markus/dev/intellij15/bin/idea.sh")
+        ]
