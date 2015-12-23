6ee4196ec5094cbd6e9388a66974cb09a89c595e
makemeunsee <makemeunsee@gmail.com>
2015-12-23 15:54:30 +0100

    battery, alternate fullscreen

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,9 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Hooks.EwmhDesktops
 
 main = do
     xmproc <- spawnPipe "~/bin/xmobar_restart"
@@ -12,16 +15,18 @@ main = do
         { terminal = "urxvt"
         , modMask = mod4Mask
         , borderWidth = 1
-        , manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $ smartBorders $ layoutHook defaultConfig
+        , layoutHook = mkToggle (single NBFULL) $ avoidStruts  $ smartBorders $ layoutHook defaultConfig
         , startupHook = setWMName "LG3D"
+        , handleEventHook = fullscreenEventHook
         }
         `additionalKeysP`
         [ ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
         , ("<XF86AudioLowerVolume>", spawn "amixer set 'Master' 3%-")
         , ("<XF86AudioRaiseVolume>", spawn "amixer set 'Master' 3%+")
         , ("<XF86Sleep>", spawn "sudo pm-suspend")
+        --, ("M-x", spawn "xrdb ~/.Xresources")
         , ("M-l", spawn "slock")
-        , ("M-f", spawn "firefox")
-        , ("M-i", spawn "~/dev/intellij15/bin/idea.sh")
+        , ("M-f", sendMessage $ Toggle NBFULL)
+        , ("M-S-f", spawn "firefox")
+        , ("M-S-i", spawn "~/dev/intellij15/bin/idea.sh")
         ]
