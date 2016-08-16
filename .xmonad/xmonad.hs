dced7193e7062bc28ef8305ced0287330b350ce6
makemeunsee <makemeunsee@gmail.com>
2016-08-16 22:10:40 +0200

    ubuntu 16.04 fix + kb layout switch

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+import Data.IORef
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
@@ -10,6 +11,7 @@ import XMonad.Layout.MultiToggle.Instances
 import XMonad.Hooks.EwmhDesktops
 
 main = do
+    kbLayoutIdRef <- newIORef 0
     xmproc <- spawnPipe "~/bin/xmobar_restart"
     xmonad $ defaultConfig
         { terminal = "urxvt"
@@ -17,7 +19,7 @@ main = do
         , borderWidth = 1
         , layoutHook = mkToggle (single NBFULL) $ avoidStruts  $ smartBorders $ layoutHook defaultConfig
         , startupHook = setWMName "LG3D"
-        , handleEventHook = fullscreenEventHook
+        , handleEventHook = mconcat [ fullscreenEventHook, docksEventHook ]
         }
         `additionalKeysP`
         [ ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
@@ -28,5 +30,17 @@ main = do
         , ("M-l", spawn "slock")
         , ("M-f", sendMessage $ Toggle NBFULL)
         , ("M-S-f", spawn "firefox")
-        , ("M-S-i", spawn "~/dev/intellij15/bin/idea.sh")
+        -- , ("M-S-i", spawn "~/dev/intellij15/bin/idea.sh")
+        , ("M-S-s", cycleKbLayout kbLayoutIdRef)
         ]
+
+layouts 0 = "us"
+layouts 1 = "ch -variant fr"
+layouts _ = layouts 0
+
+cycleKbLayout :: IORef Integer -> X ()
+cycleKbLayout ref = do
+  val <- io $ readIORef ref
+  let newVal = mod (val + 1) 2
+  io $ writeIORef ref newVal
+  spawn $ "setxkbmap -layout " ++ (layouts newVal)
