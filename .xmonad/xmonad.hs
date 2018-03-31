d2f5dcadfe2dd4004ccfe0031af89f5622b1a0f2
makemeunsee <makemeunsee@gmail.com>
2018-03-31 14:12:13 +0200

    partial merge from xps13

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,32 +9,43 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Spacing
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+randomFeh = "feh --randomize --bg-max "
+randomWallpaper = randomFeh ++ "~/Pictures/wallpapers/"
+randomInfographics = randomFeh ++ "~/Pictures/infographics/"
 
 main = do
     kbLayoutIdRef <- newIORef 0
-    xmproc <- spawnPipe "~/bin/xmobar_restart"
-    xmonad $ defaultConfig
+    _ <- spawnPipe "~/bin/xmobar_restart"
+    _ <- spawnPipe randomWallpaper
+    xmonad $ def
         { terminal = "urxvt"
         , modMask = mod4Mask
-        , borderWidth = 1
-        , layoutHook = mkToggle (single NBFULL) $ avoidStruts  $ smartBorders $ layoutHook defaultConfig
+        , borderWidth = 2
+        , normalBorderColor = "#224488"
+        , focusedBorderColor = "#ff2200"
+        , layoutHook = smartSpacing 8 $ mkToggle (single NBFULL) $ avoidStruts  $ smartBorders $ layoutHook def
         , startupHook = setWMName "LG3D"
         , handleEventHook = mconcat [ fullscreenEventHook, docksEventHook ]
         }
         `additionalKeysP`
-        [ ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
+        [ ("<XF86AudioMute>", spawn "amixer sset Master toggle")
         , ("<XF86AudioLowerVolume>", spawn "amixer set 'Master' 3%-")
         , ("<XF86AudioRaiseVolume>", spawn "amixer set 'Master' 3%+")
         , ("<XF86Sleep>", spawn "sudo pm-suspend")
         --, ("M-x", spawn "xrdb ~/.Xresources")
-        , ("M-l", spawn "slock")
+        , ("M-S-l", spawn "slock")
         , ("M-f", sendMessage $ Toggle NBFULL)
         , ("M-S-f", spawn "firefox")
-        -- , ("M-S-i", spawn "~/dev/intellij15/bin/idea.sh")
         , ("M-S-s", cycleKbLayout kbLayoutIdRef)
+        , ("M-S-i", spawn randomInfographics)
+        , ("M-S-w", spawn randomWallpaper)
         ]
 
-layouts 0 = "us"
+layouts 0 = "us -option compose:caps"
 layouts 1 = "ch -variant fr"
 layouts _ = layouts 0
 
